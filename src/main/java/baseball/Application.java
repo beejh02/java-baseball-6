64ced7441217527f7fe32b784497f83815edfad6
beejh02 <joon0448@naver.com>
2025-02-15 00:48:59 +0900

    테스트까지 진행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,7 +4,7 @@ import baseball.controller.GameController;
 
 public class Application {
     public static void main(String[] args) {
-        GameController gameController = new GameController(); // GameController 객체 생성
-        gameController.gameFlow(); // 게임 흐름 실행
+        GameController gameController = new GameController();
+        gameController.gameFlow();
     }
 }
