4bcce6f28558f9b4ad9913d4366fda8ae2fe5220
beejh02 <joon0448@naver.com>
2025-02-15 20:29:48 +0900

    indent 실수 수정

--- src/main/java/baseball/model/CompareBallsModel.java
+++ src/main/java/baseball/model/CompareBallsModel.java
@@ -7,24 +7,24 @@ public class CompareBallsModel {
 
 
 // 사용자 입력과 컴퓨터 숫자 비교
-public int[] compareNumbers(String playerInput, List<Integer> computerList) {
-    int strikeCount = 0;
-    int ballCount = 0;
+    public int[] compareNumbers(String playerInput, List<Integer> computerList) {
+        int strikeCount = 0;
+        int ballCount = 0;
 
-    String computerString = computerList.stream().map(String::valueOf).collect(Collectors.joining());
+        String computerString = computerList.stream().map(String::valueOf).collect(Collectors.joining());
 
-    for (int i = 0; i < 3; i++) {
-        if (playerInput.charAt(i) == computerString.charAt(i)) {
-            strikeCount++;
+        for (int i = 0; i < 3; i++) {
+            if (playerInput.charAt(i) == computerString.charAt(i)) {
+                strikeCount++;
+            }
         }
-    }
 
-    for (int i = 0; i < 3; i++) {
-        if (playerInput.charAt(i) != computerString.charAt(i) && computerString.contains(String.valueOf(playerInput.charAt(i)))) {
-            ballCount++;
+        for (int i = 0; i < 3; i++) {
+            if (playerInput.charAt(i) != computerString.charAt(i) && computerString.contains(String.valueOf(playerInput.charAt(i)))) {
+                ballCount++;
+            }
         }
-    }
 
-    return new int[]{strikeCount, ballCount};
-}
+        return new int[]{strikeCount, ballCount};
+    }
 }
